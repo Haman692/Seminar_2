3671034b3818cc4e07159aa393a7b178bd1a60b5
Haman692 <haman692@yandex.ru>
2022-11-25 00:46:45 +0700

    task_001 002

---
+++ task_001/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+Console.Clear();
+int randomNumber = new Random().Next(10, 100);
+Console.WriteLine("Случайное число " + randomNumber);
+
+int ostatok = randomNumber % 10;
+Console.WriteLine("Второе число " + ostatok);
+
+int delitel = randomNumber/10;
+Console.WriteLine("Первое число " + delitel);
+
+if(delitel > ostatok) Console.WriteLine("Наибольшое " + delitel);
+else Console.WriteLine("Наибольшое " + ostatok);
---
+++ task_002/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+Console.Clear();
+int randomNumber = new Random().Next(100, 999);
+Console.WriteLine("Случайное число " + randomNumber);
+
+int secondDiget = randomNumber % 10;
+
+int ferstDiget = randomNumber / 100;
+
+Console.WriteLine(ferstDiget*10+secondDiget);
+
+
