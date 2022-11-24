4f0258b91ba119cc3c79169e9fa8baaac91b1d91
Haman692 <haman692@yandex.ru>
2022-11-25 01:24:06 +0700

    task002

--- task_002/Program.cs
+++ task_002/Program.cs
@@ -4,9 +4,6 @@ int randomNumber = new Random().Next(100, 999);
 Console.WriteLine("Случайное число " + randomNumber);
 
 int secondDiget = randomNumber % 10;
-
 int ferstDiget = randomNumber / 100;
 
 Console.WriteLine(ferstDiget*10+secondDiget);
-
-
